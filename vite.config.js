3b4c1c728eabc823e1243604899fb789c4266568
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2023-04-12 08:44:36 +0200

    Add coment to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '',
   build: {
-    chunkSizeWarningLimit: 1600,
+    chunkSizeWarningLimit: 1600, // this isn't neccesery
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        subpage1: resolve(__dirname, 'subpage1/index.html'),
-        subpage2: resolve(__dirname, 'subpage2/index.html'),
+        main: resolve(__dirname, 'index.html'),               // main page
+        subpage1: resolve(__dirname, 'subpage1/index.html'),  // first subpage
+        subpage2: resolve(__dirname, 'subpage2/index.html'),  // second subpage
+        // ... and like that you can add as many subpages as you need
       },
     },
   },
