eb6e77df1db9deb93184eab5ebdd025636a1a172
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2023-04-18 20:51:09 +0200

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
+const root = resolve(__dirname, 'src'); // get absolute path to root directory
+
 export default defineConfig({
-  base: '',
+  root,     // set root directory
+  base: '', // base URL for the app, default is '/'
   build: {
-    chunkSizeWarningLimit: 1600, // this isn't neccesery
+    outDir: '../dist',            // output build directory
+    chunkSizeWarningLimit: 1600,  // this isn't neccesery
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),               // main page
-        subpage1: resolve(__dirname, 'subpage1/index.html'),  // first subpage
-        subpage2: resolve(__dirname, 'subpage2/index.html'),  // second subpage
+        main: resolve(root, 'index.html'),               // main page
+        subpage1: resolve(root, 'subpage1/index.html'),  // first subpage
+        subpage2: resolve(root, 'subpage2/index.html'),  // second subpage
         // ... and like that you can add as many subpages as you need
       },
     },
